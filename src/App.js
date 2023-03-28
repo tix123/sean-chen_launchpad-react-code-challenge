e27b00f288e7d6ab5ae39f19a0552649fba66251
Sean Chen <tix123@msn.com>
2023-03-27 18:52:33 -0600

    home basic UI complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+
+// import material UI components
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import TabContext from '@mui/lab/TabContext';
+import TabPanel from '@mui/lab/TabPanel';
+import Box from '@mui/material/Box';
+
+// import pages
+import Home from './components/Home'
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [value, setValue] = React.useState('1');
+
+    const changePage = (page) => {
+        setValue(page);
+    }
+
+    const tabPanelStyle = {
+        padding: 0
+    }
+
+
+    return (
+        <Box sx={{ width: '100%' }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Sean Chen's Code Challenge
+                    </Typography>
+                    <Stack direction="row" spacing={5}>
+                        <Button color="inherit" onClick={() => changePage("1")}>
+                            home
+                        </Button>
+                        <Button color="inherit" onClick={() => changePage("2")}>
+                            universities
+                        </Button>
+                        <Button color="inherit" onClick={() => changePage("3")}>
+                            postal lookup
+                        </Button>
+                    </Stack>
+
+                </Toolbar>
+            </AppBar>
+
+
+            <TabContext value={value}>
+                <TabPanel value="1" sx={tabPanelStyle}>
+                    <Home />
+                </TabPanel>
+                <TabPanel value="2" sx={tabPanelStyle}>
+
+                </TabPanel>
+                <TabPanel value="3" sx={tabPanelStyle}>
+
+                </TabPanel>
+            </TabContext>
+        </Box>
+
+    );
 }
 
 export default App;
