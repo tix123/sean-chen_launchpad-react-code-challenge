4265bb5603c6e109fe970b3525d4fc4c78156c13
Sean Chen <tix123@msn.com>
2023-04-06 19:14:47 -0600

    1. Move API into slice 2. Break up into smaller components

--- src/store/slices/postalSlice.js
+++ src/store/slices/postalSlice.js
@@ -1,17 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import * as Settings from "../../config/settings";
+
+export const searchPostal = createAsyncThunk(
+  "home/searchPostal",
+  async (zip) => {
+    const res = await axios.get(Settings.US_POSTAL_SERVER_URL + "/" + zip);
+    return res.data;
+  }
+);
 
 export const postalSlice = createSlice({
   name: "postal",
   initialState: {
     postalData: {},
+    status: Settings.API_IDLE,
+    error: null,
   },
-  reducers: {
-    setPostalData: (state, action) => {
-      state.postalData = action.payload;
-    },
+  extraReducers(builder) {
+    builder
+      // Status of search postal
+      .addCase(searchPostal.pending, (state, action) => {
+        state.status = Settings.API_LOADING;
+      })
+      .addCase(searchPostal.fulfilled, (state, action) => {
+        state.status = Settings.API_SUCCESSED;
+        state.postalData = action.payload;
+      })
+      .addCase(searchPostal.rejected, (state, action) => {
+        state.status = Settings.API_FAILED;
+        state.error = action.error.message;
+      });
   },
 });
-
-export const { setPostalData } = postalSlice.actions;
 
 export default postalSlice.reducer;
